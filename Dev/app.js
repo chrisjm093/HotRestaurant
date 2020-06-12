f7a812dab83a0a42b16cba2fc207cdb3d9e9e738
Christopher Marti <chrisjm093@gmail.com>
2020-06-11 20:48:40 -0700

    fixed paths

--- Dev/app.js
+++ Dev/app.js
@@ -12,15 +12,15 @@ const waitList = [];
 const reservations = [];
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"))
+    res.sendFile(path.join(__dirname, "/Templates/home.html"))
 });
 
 app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"))
+    res.sendFile(path.join(__dirname, "/Templates/reserve.html"))
 });
 
 app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"))
+    res.sendFile(path.join(__dirname, "/Templates/tables.html"))
 })
 
 app.post("/api/reservations", function(req, res) {
