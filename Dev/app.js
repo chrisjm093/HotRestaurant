f48cbb8bdc8edad6ca902513320fdf2801e83257
Heng <bunsanphe@gmail.com>
2020-06-11 20:01:20 -0700

    added edits from html

--- Dev/app.js
+++ Dev/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 
 const app = express();
@@ -8,14 +7,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log("Listening on Port! " + PORT);
 });
-=======
-
-
-
-
-
-
-
 
 
 const waitList = [];
@@ -32,4 +23,3 @@ app.get("/reserver", function(req, res) {
 app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html"))
 })
->>>>>>> 01ab8d452a2a171f0b0f3291cbb47d9f36f28b26
